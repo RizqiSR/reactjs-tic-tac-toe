73d77a8ee35b734844f9c37a80ff5fa2f381a5bf
RizqiSR <rizqi.srahmah27@gmail.com>
2023-10-11 20:46:51 +0700

    Add calculateWinner() function

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,12 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNew, setXIsNew] = useState(true);
+  console.log(xIsNew);
 
   function handleClick(index) {
-    if (squares[index]) return;
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
     const newSquares = squares.slice();
 
     newSquares[index] = xIsNew ? "X" : "O";
@@ -23,9 +26,18 @@ export default function Board() {
     setXIsNew(!xIsNew);
   }
 
+  const winner = calculateWinner(squares);
+  let status = "";
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNew ? "X" : "O"}`;
+  }
+
   return (
     <>
       <h1>Let's Play Tic Tac Toe!</h1>
+      <div className="status">{status}</div>
       <div className="board">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -39,4 +51,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
